e8ae2fd144bf9402dc3060afcafab44d2cb19bf6
Niels <niels.lohmann@gmail.com>
2013-10-30 23:25:35 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Südmensa Speiseplan
+
+Lädt den Speiseplan der großartigen [Südmensa](http://tinyurl.com/suedmensa) herunter und bietet ihn als JSON-Datei an. Diese wird unter anderem genutzt, um täglich über den Account [@Suedmensa](https://twitter.com/suedmensa) zu twittern.
+
+## Beispiel
+
+    ./getplan.py
+
+```json
+{
+  "datum": "30.10.2013", 
+  "aktion": [
+    "Coq au Vin - H\u00e4hnchen in Rotwein und Pilzen (L-)"
+  ], 
+  "pasta": [
+    "Tomatenso\u00dfe mit Rucola (vegan) (L-)", 
+    "Puten-Curry-So\u00dfe"
+  ], 
+  "vital": [
+    "Grie\u00dfsuppe mit Basilikum", 
+    "Chicken Tikka masala (ind. H\u00e4hnchen-Curry)", 
+    "Bolognese mit Rinderhackfleisch", 
+    "Nudelpfanne mit Gem\u00fcse (L-) (vegan)", 
+    "Falafel mit Dip (vegan)", 
+    "K\u00e4seschnitzel (fleischlos)"
+  ], 
+  "theke1": [
+    "Kartoffel-Gem\u00fcseauflauf"
+  ], 
+  "theke2": [
+    "Steak mit Tomate und Mozzarella \u00fcberbacken", 
+    "griechisches Hacksteak", 
+    "Fischfilet, P\u00fcree mit Sellerie (Tagestipp)"
+  ]
+}
+```
+
+## Installation
+
+Der Speiseplan läuft unter Python und nutzt [Beautiful Soup](http://www.crummy.com/software/BeautifulSoup/). Die Installation ist einfach:
+
+    virtualenv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+
+## Danksagung
+
+Das [Icon](http://thenounproject.com/noun/restaurant/#icon-No2392) von [@Suedmensa](https://twitter.com/suedmensa) wurde von [Saman Bemel-Benrud](http://thenounproject.com/samanbb/#) entworfen und als [Public Domain](http://creativecommons.org/publicdomain/zero/1.0/) veröffentlicht.
---
+++ getplan.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+import bs4
+import urllib2
+import json
+
+def getplan():
+    """
+    Returns the current menu as dictionary.
+    """
+
+    # load and parse the menu website
+    url = ("http://www.studentenwerk-rostock.de/index.php?lang=de"
+           "&mainmenue=4&submenue=47&type=details&detail1=1&detail2=8432")
+    html = urllib2.urlopen(url).read()
+    soup = bs4.BeautifulSoup(html, "lxml")
+
+    # the 21st table contains the menu
+    menu_raw = soup.find_all('table')[21]
+
+    # replace the 'VITALTHEKE' icon with text
+    pic = '<img border="0" src="../../grafiken/webseite/de/vital_theke_100.jpg"/>'
+    menu_raw = str(menu_raw).replace(pic, "VITALTHEKE")
+
+    # re-parse the edited menu
+    menu_raw = bs4.BeautifulSoup(menu_raw, "lxml")
+
+    # translate into list; remove empty lines
+    menu_list = [x for x in menu_raw.text.split('\n') if x != '']
+
+    # the structure of the return object
+    menu = {
+        "datum": menu_list[0].split()[-1],
+        "theke1": [],
+        "theke2": [],
+        "vital": [],
+        "aktion": [],
+        "pasta": []
+    }
+
+    theke = ''
+    praedikat = ''
+
+    for item in menu_list[1:]:
+        item = item.strip()
+
+        if item == 'THEKE 1':
+            theke = 'theke1'
+            praedikat = ''
+            continue
+
+        if item == 'THEKE 2':
+            theke = 'theke2'
+            praedikat = ''
+            continue
+
+        if item == 'VITALTHEKE':
+            theke = 'vital'
+            praedikat = ''
+            continue
+
+        if item == 'AKTIONSTHEKE':
+            theke = 'aktion'
+            praedikat = ''
+            continue
+
+        if item == 'PASTATHEKE':
+            theke = 'pasta'
+            praedikat = ''
+            continue
+
+        if item == 'vegan':
+            praedikat = ' (vegan)'
+            continue
+
+        if item == 'fleischlos':
+            praedikat = ' (fleischlos)'
+            continue
+
+        i = item.find("(Preisvorteil")
+        if i != -1:
+            item = item[0:i]
+
+        if "TAGESTIPP: " in item:
+            item = item[11:] + "(Tagestipp)"
+
+        menu[theke].append((item + praedikat))
+
+    return menu
+
+if __name__ == "__main__":
+    print json.dumps(getplan(), indent=2)
